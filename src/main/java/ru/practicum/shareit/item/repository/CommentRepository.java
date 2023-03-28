5a02eff4e5489049202a64f1259dcb2067532a63
i.s.melnikov <i.s.melnikov@tinkoff.ru>
2023-03-28 10:33:40 +0300

    style: убрать неиспользуемые import'ы

--- src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.item.model.Comment;
 
 import java.util.List;
--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
